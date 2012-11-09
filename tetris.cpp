9e960e61fe1b4fe0754df75f2a7713147e9c060b
DQNEO <dqneoo@gmail.com>
2012-11-10 02:44:31 +0900

    15:50 構造体にパラメータをセット(ただしコンパイエラー)

--- tetris.cpp
+++ tetris.cpp
@@ -1,6 +1,23 @@
 #include <windows.h>
 
 int WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdLine, int cmdShow) {
+    WNDCLASSEX wc;
+    static LPCTSTR pClassName = "NicoNicoProgramming2";  // クラス名
+
+    wc.cbSize        = sizeof(WNDCLASSEX);               // 構造体サイズ
+    wc.style         = CS_HREDRAW | CS_VREDRAW;          // クラススタイル
+    wc.lpfnWndProc   = (WNDPROC)WndProc;
+    wc.cbClsExtra    = 0;                                // 補足メモリブロック
+    wc.cbWndExtra    = 0;                                // 　のサイズ
+    wc.hInstance     = hInst;                            // インスタンス
+    wc.hIcon         = NULL;                             // アイコン
+    wc.hCursor       = LoadCursor(NULL,IDC_ARROW);       // カーソル
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);         // 背景色
+    wc.lpszMenuName  = NULL;                             // メニュー
+    wc.lpszClassName = pClassName;                       // クラス名
+    wc.hIconSm       = NULL;                             // 小さいアイコン
+
+    if (!RegisterClassEx(&wc)) return FALSE;             // 登録
 
     return 0;
 }
